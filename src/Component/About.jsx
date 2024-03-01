d9d0cb7dca8cde9caf643bc91e269ee48fcc4009
SamiSelx <samifcb14@gmail.com>
2024-03-01 15:44:51 -0800

    updated

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -1,11 +1,13 @@
+import { useState } from "react"
 
 export default function About(){
+    const [isOpen,setIsOpen] = useState(false)
     return(
         <div className="about">
             <div className="container">
                 <h2 className="title">ABOUT MOURAFIK.</h2>
                 <p>Monitor and gain insights into your health data to improve your overall well-being.</p>
-
+                
             </div>
         </div>
     )
